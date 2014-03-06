87bd7559a2903681a9a6756c7e9ea6b3a91517ef
David Whitney <david@davidwhitney.co.uk>
2014-03-06 00:17:18 +0000

    Testing configuration API and fluent feature storage.

--- ReallySimpleFeatureToggle.Test.Unit/ReallySimpleFeatureToggleConfigurationApiTests.cs
+++ ReallySimpleFeatureToggle.Test.Unit/ReallySimpleFeatureToggleConfigurationApiTests.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using Moq;
 using NUnit.Framework;
 using ReallySimpleFeatureToggle.Configuration;
+using ReallySimpleFeatureToggle.Configuration.FeatureNotConfiguredBehaviours;
 
 namespace ReallySimpleFeatureToggle.Test.Unit
 {
@@ -34,6 +36,44 @@ namespace ReallySimpleFeatureToggle.Test.Unit
             _config.WithFeatureConfigurationSource(mockSource.Object, new TimeSpan(200));
 
             Assert.That(_config.FeatureConfigRepository, Is.TypeOf<CachingFeaturesRepository>());
+        }
+
+        [Test]
+        public void WhenFeatureRequestedIsNotConfigured_PassedStrategy_WiresUp()
+        {
+            var mock = new Mock<IFeatureNotConfiguredBehaviour>();
+
+            _config.WhenFeatureRequestedIsNotConfigured(mock.Object);
+
+            Assert.That(_config.FeatureNotConfiguredBehaviour, Is.EqualTo(mock.Object));
+        }
+
+        [Test]
+        public void GlobalAvailabilityRules_Modified_MutatesGlobalRules()
+        {
+            _config.GlobalAvailabilityRules(x => x.Clear());
+
+            Assert.That(_config.DefaultAvailabilityRules, Is.Empty);
+        }
+
+        [Test]
+        public void GlobalOverrides_Modified_MutatesGlobalRules()
+        {
+            _config.GlobalOverrides(x => x.Clear());
+
+            Assert.That(_config.OverrideRules, Is.Empty);
+        }
+
+        [Test]
+        public void WithFeatures_StoresFluentlyCreatedFeaturesInInMemoryRepository()
+        {
+            _config.WithFeatures(x => x.Add(Feature.Called("MyFeature").ThatIsEnabled()));
+
+            var features = _config.FeatureConfigRepository.GetFeatureSettings();
+
+            Assert.That(features.Count, Is.EqualTo(1));
+            Assert.That(features.First().Name, Is.EqualTo("MyFeature"));
+            Assert.That(features.First().State, Is.EqualTo(State.Enabled));
         }
     }
 }
