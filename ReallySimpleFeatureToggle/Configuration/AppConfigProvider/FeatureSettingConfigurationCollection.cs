bd67841714d112133196f5a6c195e0c2201f00a0
David Whitney <david@davidwhitney.co.uk>
2014-03-06 00:48:08 +0000

    Removed unneeded clear / add config syntax. Totally unused.

--- ReallySimpleFeatureToggle/Configuration/AppConfigProvider/FeatureSettingConfigurationCollection.cs
+++ ReallySimpleFeatureToggle/Configuration/AppConfigProvider/FeatureSettingConfigurationCollection.cs
@@ -8,30 +8,7 @@ namespace ReallySimpleFeatureToggle.Configuration.AppConfigProvider
         {
             get { return ConfigurationElementCollectionType.AddRemoveClearMap; }
         }
-
-        public FeatureConfigurationElement this[int index]
-        {
-            get { return (FeatureConfigurationElement)BaseGet(index); }
-            set
-            {
-                if (BaseGet(index) != null)
-                {
-                    BaseRemoveAt(index);
-                }
-                BaseAdd(index, value);
-            }
-        }
-
-        public void Add(FeatureConfigurationElement element)
-        {
-            BaseAdd(element);
-        }
-
-        public void Clear()
-        {
-            BaseClear();
-        }
-
+        
         protected override ConfigurationElement CreateNewElement()
         {
             return new FeatureConfigurationElement();
@@ -40,21 +17,6 @@ namespace ReallySimpleFeatureToggle.Configuration.AppConfigProvider
         protected override object GetElementKey(ConfigurationElement element)
         {
             return ((FeatureConfigurationElement)element).Name;
-        }
-
-        public void Remove(FeatureConfigurationElement element)
-        {
-            BaseRemove(element.Name);
-        }
-
-        public void Remove(string name)
-        {
-            BaseRemove(name);
-        }
-
-        public void RemoveAt(int index)
-        {
-            BaseRemoveAt(index);
         }
     }
 }
