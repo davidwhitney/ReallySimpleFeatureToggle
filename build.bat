6dd59f5a5ac969c17f24b0ef895cb45c45c2cae7
David Whitney <david@davidwhitney.co.uk>
2015-06-06 17:46:59 +0100

    build script reinstated

--- build.bat
+++ build.bat
@@ -1,2 +1,16 @@
 C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "ReallySimpleFeatureToggle.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
+
+.nuget\nuget pack "ReallySimpleFeatureToggle\ReallySimpleFeatureToggle.csproj" -Properties Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+.nuget\nuget pack "ReallySimpleFeatureToggle.Web\ReallySimpleFeatureToggle.Web.csproj" -Properties Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+.nuget\nuget pack "ReallySimpleFeatureToggle.Web.Mvc\ReallySimpleFeatureToggle.Web.Mvc.csproj" -Properties Configuration=Release
+if %errorlevel% neq 0 exit /b %errorlevel%
+
+
+mkdir NuGetPackage
+copy *.nupkg NuGetPackage\*.nupkg
+del *.nupkg
