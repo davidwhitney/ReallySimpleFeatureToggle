ea36523cd40c301d8989ca4f7332566f53f293d3
David Whitney <david@davidwhitney.co.uk>
2015-08-06 17:45:32 +0100

    Removed paths from build.bat

--- build.bat
+++ build.bat
@@ -1,5 +1,5 @@
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release "ReallySimpleFeatureToggle.sln"
+MSBuild.exe /m:8 /p:Configuration=Release "ReallySimpleFeatureToggle.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
 
-C:\Windows\Microsoft.NET\Framework\v4.0.30319\MSBuild.exe /m:8 /p:Configuration=Release40 "ReallySimpleFeatureToggle.sln"
+MSBuild.exe /m:8 /p:Configuration=Release40 "ReallySimpleFeatureToggle.sln"
 if %errorlevel% neq 0 exit /b %errorlevel%
